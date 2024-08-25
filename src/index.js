561f103ce9a397b76ce48ebebf1807dcd820e399
Bander Btati <bander.mohammed.2000@gmail.com>
2024-08-25 12:43:16 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
+//نلاحظ اننا لم نضيف بالاخير امتداد جافاسكريبت, وسواء اضفناه او لا فالنتيجة نفسها وذلك ربما بسبب اننا في ملف الجافاسكريبت ليس من الضروري اضافته
+import "@fortawesome/fontawesome-free/js/all.min";
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
