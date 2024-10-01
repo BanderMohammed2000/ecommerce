aef6dab344d2ef251fb600d1e96901093bcd1347
Bander Btati <bander.mohammed.2000@gmail.com>
2024-10-01 23:51:18 +0300

    product page done

--- src/index.js
+++ src/index.js
@@ -19,3 +19,27 @@ document.querySelectorAll(".add-to-cart-btn").forEach((item) => {
 
 document.getElementById("copyright").innerHTML =
   "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear();
+
+document
+  .querySelectorAll('.size-option input[type="radio"]')
+  .forEach((item) => {
+    item.addEventListener("change", () => {
+      document.querySelectorAll(".size-option").forEach((i) => {
+        i.classList.remove("active");
+      });
+      // للوصول الى الاب الثاني للعنصر
+      item.parentNode.parentNode.classList.add("active");
+    });
+  });
+
+document
+  .querySelectorAll('.color-option input[type="radio"]')
+  .forEach((item) => {
+    item.addEventListener("change", () => {
+      document.querySelectorAll(".color-option").forEach((i) => {
+        i.classList.remove("active");
+      });
+      // للوصول الى الاب الثاني للعنصر
+      item.parentNode.parentNode.classList.add("active");
+    });
+  });
