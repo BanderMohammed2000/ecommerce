7fcedd15ea919b052cb31fb3b680ba8e2029181a
Bander Btati <bander.mohammed.2000@gmail.com>
2024-10-07 20:54:07 +0300

    contact page done

--- webpack.config.js
+++ webpack.config.js
@@ -96,6 +96,10 @@ module.exports = {
       filename: "search.html",
       template: "./src/search.html",
     }),
+    new HtmlWebpackPlugin({
+      filename: "contact.html",
+      template: "./src/contact.html",
+    }),
     new MiniCssExtractPlugin({
       filename: "css/style.css",
     }),
