d9459618cbc85c553119f1191ffae41ea8549d7f
Bander Btati <bander.mohammed.2000@gmail.com>
2025-01-25 12:07:57 +0300

    update webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     app: "./src/index.js",
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "main.js",
   },
   devServer: {
